6f7e94829f5e4c73960077edda7db655892ffadb
Elisabethdemaio <elisabeth.demaio@gmail.com>
2023-01-12 16:53:18 +0100

    changed setup

--- setup.py
+++ setup.py
@@ -1,26 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='geoserver',
+    name='gsconfig_edm',
     version='0.1.0',
-    description='A Geoserver base package',
-    url='https://github.com/shuds13/pyexample',
-    author='Stephen Hudson',
-    author_email='shudson@anl.gov',
-    license='BSD 2-clause',
-    packages=['pyexample'],
+    description='A Geoserver base configuration package ',
+    url='https://github.com/ElisabethDemaio/gsconfig.git',
+    author='Liz',
+    author_email='',
+    license='',
+    packages=find_packages(),
     install_requires=[
                       ],
-
-    classifiers=[
-        'Development Status :: 1 - Planning',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-    ],
 )
